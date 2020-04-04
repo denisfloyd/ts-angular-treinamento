27326b028ac9e34adbdf2257add4e8b62be666f5
denismend <denisladeira1@gmail.com>
2020-04-04 12:21:50 -0300

    refactor fonts

--- todomain.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var todoclasses_1 = require("./todoclasses");
-var list = new todoclasses_1["default"]();
-list.addTodo({
-    title: 'Learn Typescript',
-    isDone: false
-});
-console.log(list);
